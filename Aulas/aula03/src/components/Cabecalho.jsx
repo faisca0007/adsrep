5744bd830d042014867c6198cdea756bdbf88c5f
pedro <pedrogabrielps@hotmail.com>
2024-09-02 20:20:45 -0300

    Adicionando arquivos da pratica04

---
+++ Aulas/aula03/src/components/Cabecalho.jsx
@@ -0,0 +1,22 @@
+function Cabecalho(){
+    return(
+     <header>
+       <h1>Minha Aplicação</h1>
+     </header>
+    );
+ 
+ }
+ 
+ 
+ 
+ 
+ function App(){
+   return(
+   <>
+   <Cabecalho  />
+   <p> Bem vindo ! </p>
+   </>
+   );
+ }
+
+ export default Cabecalho;
