3d51e1f844b0ce575010cee22e92555a4b597270
pedro <pedrogabrielps@hotmail.com>
2024-10-10 20:47:58 -0300

    Aula08

---
+++ Aulas/aula08/src/pages/FormTarefa.jsx
@@ -0,0 +1,10 @@
+function FormTarefa() {
+   return( 
+   <form>
+      <input type="text" placeholder="Digite uma tarefa" />
+      <button type="submit">Adicionar</button>
+   </form>
+ );
+
+}
+export default FormTarefa;
